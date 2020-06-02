a3603a79dbb0354931e2489e9af5aad3049b0683
janriedler <jan@riedler.online>
2020-06-02 13:38:57 +0200

    Initial commit

---
+++ src/main/java/de/unipassau/fim/projekt21/Projekt21Application.java
@@ -0,0 +1,13 @@
+package de.unipassau.fim.projekt21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt21Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/unipassau/fim/projekt21/Projekt21ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unipassau.fim.projekt21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
