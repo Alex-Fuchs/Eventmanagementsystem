595b5e6bf22a566580057b4319c1e5e72abf3468
janriedler <jan@riedler.online>
2020-06-21 13:40:44 +0200

    change name to group 40

--- src/test/java/de/unipassau/fim/projekt40/Projekt40ApplicationTests.java
+++ src/test/java/de/unipassau/fim/projekt40/Projekt40ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Projekt21ApplicationTests {
+class Projekt40ApplicationTests {
 
 	@Test
 	void contextLoads() {
