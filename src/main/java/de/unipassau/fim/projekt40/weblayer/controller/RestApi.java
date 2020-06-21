527d5d5841769572712c2738a7c780c5242f7f71
Lukas Ellinger <lu.ellinger@gmx.de>
2020-06-21 17:27:03 +0200

    Bonus: RestAPI

---
+++ src/main/java/de/unipassau/fim/projekt40/weblayer/controller/RestApi.java
@@ -0,0 +1,36 @@
+package de.unipassau.fim.projekt40.weblayer.controller;
+
+import com.google.gson.Gson;
+
+import de.unipassau.fim.projekt40.data_access_layer.data_access_object.Event;
+import de.unipassau.fim.projekt40.data_access_layer.repository.EventRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class RestApi {
+
+    private EventRepository repository;
+
+    @Autowired
+    public RestApi(EventRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("events")
+    @ResponseBody
+    public String json(@RequestParam String sizeString) {
+        List<Event> events = repository.findAllSort();
+        int size = Integer.parseInt(sizeString);
+        while (events.size() > size) {
+            events.remove(0);
+        }
+        return new Gson().toJson(events);
+    }
+}
