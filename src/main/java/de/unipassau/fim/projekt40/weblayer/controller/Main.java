13a5bcdd87a8b7af9404bd622ae7c58d692b9905
Lukas Ellinger <lu.ellinger@gmx.de>
2020-06-21 15:05:42 +0200

    Added event.html and updated style.css

--- src/main/java/de/unipassau/fim/projekt40/weblayer/controller/Main.java
+++ src/main/java/de/unipassau/fim/projekt40/weblayer/controller/Main.java
@@ -1,7 +1,14 @@
 package de.unipassau.fim.projekt40.weblayer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class Main {
@@ -14,5 +21,10 @@ public class Main {
     @GetMapping("add")
     public String addEvent() {
         return "add";
+    }
+
+    @GetMapping("event")
+    public String showEvent() {
+        return "event";
     }
 }
