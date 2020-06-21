c3dd12369005da3ffdcaa74a0690e31bc965cd54
janriedler <jan@riedler.online>
2020-06-21 14:40:55 +0200

    add add.html, add-Controller, and pers. DB

--- src/main/java/de/unipassau/fim/projekt40/weblayer/controller/Main.java
+++ src/main/java/de/unipassau/fim/projekt40/weblayer/controller/Main.java
@@ -1,11 +1,18 @@
 package de.unipassau.fim.projekt40.weblayer.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class Main {
 
     @GetMapping()
     public String showAll() {
         return "index";
+    }
+
+    @GetMapping("add")
+    public String addEvent() {
+        return "add";
     }
 }
