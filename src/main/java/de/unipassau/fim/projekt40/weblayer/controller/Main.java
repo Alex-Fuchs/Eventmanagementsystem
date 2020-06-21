76de94a3428eb29ba1eda1f71e08b7946e429987
Alex-Fuchs <fuchs.ali@gmail.com>
2020-06-21 17:07:07 +0200

    Added Session Cookies for Voting.

--- src/main/java/de/unipassau/fim/projekt40/weblayer/controller/Main.java
+++ src/main/java/de/unipassau/fim/projekt40/weblayer/controller/Main.java
@@ -1,23 +1,27 @@
 package de.unipassau.fim.projekt40.weblayer.controller;
 
 import de.unipassau.fim.projekt40.data_access_layer.data_access_object.Event;
-import de.unipassau.fim.projekt40.data_access_layer.data_access_object.EventType;
 import de.unipassau.fim.projekt40.data_access_layer.repository.EventRepository;
+import de.unipassau.fim.projekt40.data_access_layer.data_access_object.EventType;
 import de.unipassau.fim.projekt40.data_access_layer.repository.EventTypeRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.model.IModel;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Controller
-public class Main {
+class Main {
 
     private EventRepository eventRepository;
     private EventTypeRepository eventTypeRepository;
@@ -30,22 +34,32 @@ public class Main {
 
     @GetMapping()
     public String showAll(HttpServletRequest request, Model model) {
-        doPreparations(request, eventRepository.findAllSort(), model);
-        return "index";
+        List<Event> events = new ArrayList<>();
+        for (Event event: eventRepository.findAllSort()) {
+            if (EventRepository.checkDateIsInFuture(event.getDatum())) {
+                events.add(event);
+            }
+        }
+        doPreparations(request, events, model);
+        return "verlist";
     }
 
     @GetMapping("add")
     public String addEvent(Model model) {
-        setEventTypes(model);
+        model.addAttribute("eventTypes", eventTypeRepository.findAll());
         return "add";
     }
 
-    @GetMapping("event")
-    public String showEvent(HttpServletRequest request, Model model, @RequestParam String id) {
-        List<Event> events = new ArrayList<>();
-        events.add(eventRepository.findById(Integer.parseInt(id)));
+    @GetMapping("sort")
+    public String ShowAllWithEventType(HttpServletRequest request, Model model, @RequestParam String sort) {
+        List<Event> events;
+        if (sort.equals("Alle (auch Vergangenheit)")) {
+            events = eventRepository.findAllSort();
+        } else {
+            events = eventRepository.findByEventType(sort);
+        }
         doPreparations(request, events, model);
-        return "event";
+        return "verlist";
     }
 
     @GetMapping("search")
@@ -58,19 +72,69 @@ public class Main {
             }
         }
         doPreparations(request, searchedEvents, model);
-        return "index";
+        return "verlist";
     }
 
-    @GetMapping("sort")
-    public String ShowAllWithEventType(HttpServletRequest request, Model model, @RequestParam String sort) {
-        List<Event> events;
-        if (sort.equals("Alle (auch Vergangenheit)")) {
-            events = eventRepository.findAllSort();
-        } else {
-            events = eventRepository.findByEventType(sort);
+    @GetMapping("vote")
+    @ResponseBody
+    public String vote (HttpServletRequest request, HttpServletResponse response, @RequestParam String id, @RequestParam String ranking) {
+        int value = Integer.parseInt(ranking);
+        if (Math.abs(value) == 1) {
+            int oldVoting = getOldVoting(request, id);
+            if (oldVoting == value) {
+                response.addCookie(new Cookie(id, "0"));
+                value = -1 * oldVoting;
+            } else {
+                response.addCookie(new Cookie(id, ranking));
+                value += -1 * oldVoting;
+            }
+            long tmp = Long.parseLong(id);
+            eventRepository.vote(tmp, value);
+            return "\"<script LANGUAGE='JavaScript'>\n" +
+                    "    window.alert('Vielen Dank für deinen Vote');\n" +
+                    "    window.location.href='/';\n" +
+                    "    </script>\"" +
+                    "Vielen Dank für deinen Vote";
         }
+        return "\"<script LANGUAGE='JavaScript'>\n" +
+                "    window.alert('Etwas ist mit der Session schief gelaufen!');\n" +
+                "    window.location.href='/';\n" +
+                "    </script>\"" +
+                "Etwas ist mit der Session schief gelaufen!";
+    }
+
+    @GetMapping("event")
+    public String showEvent(HttpServletRequest request, Model model, @RequestParam String id) {
+        List<Event> events = new ArrayList<>();
+        events.add(eventRepository.findById(Integer.parseInt(id)));
         doPreparations(request, events, model);
-        return "index";
+        return "event";
+    }
+
+    private void doPreparations(HttpServletRequest request, List<Event> events, Model model) {
+        getLast20(events);
+        setEventTypes(model);
+        ArrayList<Long> upVote = new ArrayList<>();
+        ArrayList<Long> downVote = new ArrayList<>();
+        setUpDownVoteLists(request, upVote, downVote);
+
+        model.addAttribute("upVote", upVote);
+        model.addAttribute("downVote", downVote);
+        model.addAttribute("top3", getTop3());
+        model.addAttribute("events", events);
+    }
+
+    private void setUpDownVoteLists(HttpServletRequest request, ArrayList<Long> upVote, ArrayList<Long> downVote) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie: cookies) {
+                if (cookie.getValue().equals("1")) {
+                    upVote.add(Long.parseLong(cookie.getName()));
+                } else if (cookie.getValue().equals("-1")){
+                    downVote.add(Long.parseLong(cookie.getName()));
+                }
+            }
+        }
     }
 
     private void setEventTypes(Model model) {
@@ -79,12 +143,23 @@ public class Main {
         model.addAttribute("eventTypes", eventTypes);
     }
 
-    private void doPreparations(HttpServletRequest request, List<Event> events, Model model) {
-        getLast20(events);
-        setEventTypes(model);
-
-        model.addAttribute("top3", getTop3());
-        model.addAttribute("events", events);
+    private int getOldVoting(HttpServletRequest request, String id) {
+        Cookie[] cookies = request.getCookies();
+        Cookie voted = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(id)) {
+                    voted = cookie;
+                }
+            }
+        }
+        if (voted != null) {
+            int votedValue = Integer.parseInt(voted.getValue());
+            if (Math.abs(votedValue) == 1) {
+                return votedValue;
+            }
+        }
+        return 0;
     }
 
     private List<Event> getTop3() {
